98c547f5af76363259cfdcf0225c6d5ab1d154a7
Michael Tsai <mingxcv@gmail.com>
2023-08-18 21:59:13 +0800

    feat: remove all analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,9 @@
 <script lang="ts">
-	import { browser, dev } from '$app/environment';
-	import { page } from '$app/stores';
-	import Analytics from '$lib/components/Analytics.svelte';
 	import Header from '$lib/components/Header.svelte';
-	import { webVitals } from '$lib/vitals';
-	import { inject } from '@vercel/analytics';
 	import 'overlayscrollbars/overlayscrollbars.css';
 	import './styles.css';
-
-	let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
-
-	$: if (browser && analyticsId) {
-		webVitals({
-			path: $page.url.pathname,
-			params: $page.params,
-			analyticsId
-		});
-	}
-
-	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
-<Analytics />
 <Header />
 <div class="page-container">
 	<main class="page-wrapper">
--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,5 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-	define: {
-		'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
 	}
 });
