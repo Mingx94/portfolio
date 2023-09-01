ac2df04264f70a19436722e9d8fc919cb327c46b
Michael Tsai <mingxcv@gmail.com>
2023-09-02 00:14:59 +0800

    feat: update about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,8 +1,6 @@
 <script>
-	import me from '$lib/assets/me.jpg';
-
 	let title = '關於我 | Coding Shutter';
-	let description = 'Michael Tsai, 前端工程師 | Coding Shutter';
+	let description = '關於我 | Coding Shutter';
 </script>
 
 <svelte:head>
@@ -16,60 +14,18 @@
 	<meta property="og:url" content="https://coding-shutter.blog/" />
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
-	<meta property="og:image" content={me} />
-	<meta property="og:width" content="300" />
-	<meta property="og:height" content="300" />
-
-	<!-- Twitter Meta Tags -->
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:image" content={me} />
 </svelte:head>
 
-<article>
-	<h1>關於我</h1>
+<section class="max-w-800px mx-auto px-20px md:px-40px py-40px md:py-80px">
+	<article>
+		<div>
+			<h1 class="text-center text-24px font-600 mb-20px">關於我</h1>
 
-	<div class="avatar">
-		<img class="image-cropped-transform" src={me} alt="Michael Tsai" />
-	</div>
-
-	<p>
-		我是 Michael Tsai 前端工程師，喜好攝影和公路車。 目前使用的相機是 Sony A7C， 使用的鏡頭為 Sony
-		FE 50mm F2.5 G 和 Tamron 28-200mm F2.8-5.6。 歡迎各位一起交流。
-	</p>
-</article>
-
-<style>
-	.avatar {
-		position: relative;
-		width: 300px;
-		aspect-ratio: 1/1;
-		border-radius: 999px;
-		overflow: hidden;
-		/* fix safari overflow */
-		isolation: isolate;
-
-		margin: 0 auto;
-	}
-	.image-cropped-transform {
-		position: absolute;
-		top: 0;
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-		left: 0;
-		object-position: 55%;
-		transform: scale(1.5);
-	}
-	article {
-		max-width: 600px;
-		margin: 0 auto;
-		padding: 1rem;
-		text-align: center;
-	}
-	p {
-		line-height: 1.6;
-		margin: 1rem auto;
-		max-width: 400px;
-		text-align: left;
-	}
-</style>
+			<p class="md:text-center text-18px line-height-30px">
+				我是 Michael Tsai，本業前端工程師，業餘攝影愛好者。
+				<br />
+				<br />目前使用的相機為 Sony <b>α7C</b>。 較常拍風景，人像攝影還在摸索中。
+			</p>
+		</div>
+	</article>
+</section>
