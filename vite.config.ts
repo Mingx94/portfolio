e001c9fcf5dc91794c9f871c97fe7a9079ab8f1e
Michael Tsai <mingxcv@gmail.com>
2023-11-13 20:53:05 +0800

    chore: remove unused files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import UnoCSS from 'unocss/vite';
 
 export default defineConfig({
-	plugins: [UnoCSS(), sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	plugins: [UnoCSS(), sveltekit()]
 });
