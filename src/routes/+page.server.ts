7557cdb54ec9e1b81a79ebd6d473b6b03fc25e7c
Michael Tsai <mingxcv@gmail.com>
2023-12-11 21:51:14 +0800

    fix: temp hide album

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,5 +4,17 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
 	const { photosets } = await FlickrApi.getAlbums(FLICKR_USER_ID);
-	return { photosets };
+
+	const photoset = photosets.photoset.filter((album) => {
+		return (
+			album.title._content !== 'A7C' &&
+			album.title._content !== '50 2.5g' &&
+			album.title._content !== 'A063'
+		);
+	});
+
+	return {
+		...photosets,
+		photoset,
+	};
 };
