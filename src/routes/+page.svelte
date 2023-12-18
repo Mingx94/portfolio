0467369a720edc9f6e71d167b52c99563ea418ad
Michael Tsai <mingxcv@gmail.com>
2023-12-18 22:50:46 +0800

    fix: update size of text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,10 +28,10 @@
 <section
 	class="max-w-1400px mx-auto px-20px xl:px-40px font-400 min-h-inherit flex justify-center flex-col "
 >
-	<h1 class="text-60px mb-24px text-blueGray-800">
+	<h1 class="text-48px md:text-60px mb-12px md:mb-24px text-blueGray-800">
 		Hi, I'm <span class="text-nowrap">Michael Tsai.</span>
 	</h1>
-	<p class="text-36px text-fuchsia-600 mb-24px">A Front-end engineer and photography enthusiast.</p>
+	<p class="text-24px md:text-36px text-fuchsia-600 mb-12px md:mb-24px">A Front-end engineer and photography enthusiast.</p>
 
 	<!-- External links -->
 	<ul class="flex gap-24px sm:gap-16px">
