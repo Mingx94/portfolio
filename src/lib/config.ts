d2cee981150d0a9de680452c4e5ce4ac018ca4a5
Michael Tsai <mingxcv@gmail.com>
2024-02-19 17:15:55 +0800

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,4 +4,4 @@ export const title = 'Michael Tsai 📷 前端 / 攝影';
 
 export const description = "前端 / 攝影部落格";
 
-export const url = dev ? 'http://localhost:5173' : 'https://coding-shutter.blog'
+export const url = dev ? 'http://localhost:5173' : 'https://vartifact.cc'
