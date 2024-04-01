63562d9662e0ee3ebbbb06fae03060bab5c28ff0
Michael Tsai <mingxcv@gmail.com>
2024-04-01 23:12:57 +0800

    update layout image style

--- src/lib/components/layout-images.svelte
+++ src/lib/components/layout-images.svelte
@@ -4,20 +4,14 @@
 	import Image from './image.svelte';
 
 	export let images: GalleryItem[] = [];
-
-	const boxSpacing = 15;
-	let containerWidth = 0;
-
-	$: layout = images.map((img) => (img.width > img.height ? 2 : 1));
 </script>
 
-<div class="gallery" bind:clientWidth={containerWidth} style:--gap={boxSpacing}>
+<div class="gallery">
 	{#each images as img, i}
 		{@const thumb = img.thumbnail ?? img}
 		<a
 			href="{$page.url.pathname}?index={i}"
-			style:--column={layout[i]}
-			class="shadow-sm bg-gray-100 inline-block rounded-md overflow-hidden isolate"
+			class="inline-block aspect-ratio-1 flex items-center justify-center group overflow-hidden"
 		>
 			<Image
 				src={thumb.src}
@@ -25,7 +19,7 @@
 				width={thumb.width}
 				height={thumb.height}
 				loading="lazy"
-				class="object-cover w-full h-full rounded-md"
+				class="object-contain m-4px max-h-[calc(100%-16px)] max-w-[calc(100%-16px)] group-hover:scale-108 transition-transform duration-300"
 			/>
 		</a>
 	{/each}
@@ -34,21 +28,13 @@
 <style>
 	.gallery {
 		display: grid;
-		grid-template-columns: 1fr;
-		gap: calc(var(--gap) * 1px);
-	}
-
-	.gallery a {
-		grid-column: span 1;
+		grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+		gap: 2px;
 	}
 
 	@media (min-width: 768px) {
 		.gallery {
-			grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-		}
-
-		.gallery a {
-			grid-column: span var(--column);
+			grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
 		}
 	}
 </style>
